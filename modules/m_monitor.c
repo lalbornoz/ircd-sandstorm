7ab4d7de11804452f3669b84af0cbddd03079796
Aaron Sethman (androsyn) <androsyn@ratbox.org>
2015-10-11 13:56:06 +0000

    actually free_monitor on delete
    git-svn-id: http://svn.ratbox.org/svnroot/ircd-ratbox/branches/RATBOX_3_0@28735 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_monitor.c
+++ modules/m_monitor.c
@@ -201,6 +201,7 @@ del_monitor(struct Client *client_p, const char *nicks)
 
 		rb_dlinkFindDestroy(client_p, &monptr->users);
 		rb_dlinkFindDestroy(monptr, &client_p->localClient->monitor_list);
+		free_monitor(monptr);
 	}
 }
 
